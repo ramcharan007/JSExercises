8c8fc0ac7e74d26e9f6b5880b5beb779ff184fa3
Ravi Teja Akella <ravi.akella@apxor.com>
2022-11-15 13:04:03 +0530

    Exercise-32 API change

--- src/exercises/32/exercise.js
+++ src/exercises/32/exercise.js
@@ -1,11 +1,37 @@
-// Create a currency convertor
-// - Create a CurrencyConvertor class
-// - Use Promises to achieve this.
-// - Use the following API : https://rapidapi.com/warting/api/currency-converter/
+// Get the details of an airport
+// - Create an Airport class
+// - Have a getInfoFromIATA() method on the Airport class which returns an airport details given its iata code
+// - Have a getInfoFromICAO() method on the Airport class which returns an airport details given its icao code
+// - Use Promises for async calls.
+// - Use the following API : https://rapidapi.com/Active-api/api/airport-info
 
 // Example :
 
-// CurrencyConvertor.convert(1,"USD","INR") should return ~83.5
+// Airport.getInfoFromIATA("HYD")
+// Airport.getInfoFromICAO("HYD")
 
-// Declaration of convert
-// function convert(amount:number,sourceCurrency:string,targetCurrency:string)
+// Returns
+/*
+{
+    "id": 3081,
+    "iata": "HYD",
+    "icao": "VOHS",
+    "name": "Rajiv Gandhi International Airport",
+    "location": "Hyderabad, Telangana, India",
+    "street_number": "",
+    "street": "",
+    "city": "Hyderabad",
+    "county": "Ranga Reddy",
+    "state": "Telangana",
+    "country_iso": "IN",
+    "country": "India",
+    "postal_code": "500409",
+    "phone": "+91 40 6654 6370",
+    "latitude": 17.240263,
+    "longitude": 78.42938,
+    "uct": 330,
+    "website": "http://www.hyderabad.aero/"
+}
+
+
+*/
