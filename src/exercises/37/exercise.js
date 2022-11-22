5407d79bd001841bb175592e8bf6aa756a64f1af
Ravi Teja Akella <ravi.akella@apxor.com>
2022-11-22 12:10:53 +0530

    Exercise-37

---
+++ src/exercises/37/exercise.js
@@ -0,0 +1,9 @@
+/*
+Make this a node Project
+Install jest
+Write unit tests for all the exercises.
+While writing test cases keep these in mind
+1. Unit Test are not just for code coverage.
+2. Do not write unit tests for the sake of writing. Cover the important and corner cases.
+3. Maintaining should never be an added burden to the team.  
+*/
