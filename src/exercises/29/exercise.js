9aa2fca3ed0f5e2c22e63b6827a4a1bc753e8c1f
Ramcharan <chikkamramcharan@gmail.com>
2023-01-25 18:42:11 +0530

    29 completed

--- src/exercises/29/exercise.js
+++ src/exercises/29/exercise.js
@@ -1,6 +1,12 @@
 // - Explain currying.
 // - Convert the below function to curry function
 
-function calculateVolume(length, breadth, height) {
-  return length * breadth * height;
+function calculateVolume(length) {
+  return function(breadth){
+    return function(height){
+      return length*breadth*height;
+    }
+  
+  } 
 }
+console.log(calculateVolume(2)(3)(9));
