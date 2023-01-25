18e17d95a2cf1e76432dc32a848de2cd1c0537f8
Ramcharan <chikkamramcharan@gmail.com>
2023-01-25 15:49:01 +0530

    24 completed

--- src/exercises/24/exercise.js
+++ src/exercises/24/exercise.js
@@ -1,4 +1,5 @@
 // Read Java Script coding style guide.
 // - Share the resource link here.
+// https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript
 // - Update all the exercises you have finished above by following the style guide.
 // From the next exercise write your code following the style guide
