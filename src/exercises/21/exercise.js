9a7b7f2caf7c8e714c9e37190552cffe18277b4a
Ramcharan <chikkamramcharan@gmail.com>
2023-01-25 12:17:49 +0530

    21 completed

--- src/exercises/21/exercise.js
+++ src/exercises/21/exercise.js
@@ -1,1 +1,8 @@
 // What are the different levels of logs and how do we print them to the console?
+/**
+ * log
+ * info
+ * error
+ * warn
+ * To print them to console we use console.log(),console.info(),console.error(),console.warn().
+ */
