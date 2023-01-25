c1862adeaca2b47c88a593f88735974f4e185c20
Ramcharan <chikkamramcharan@gmail.com>
2023-01-25 16:10:09 +0530

    25 completed

--- src/exercises/25/exercise.js
+++ src/exercises/25/exercise.js
@@ -1,1 +1,14 @@
 // Read what is a closure and give an example here
+function closureExample()
+{
+    var b = 0;
+    function inner(){
+        return b+=1;
+    }
+    return inner;
+}
+var getInner = closureExample();  
+  
+console.log(getInner());
+console.log(getInner());
+console.log(getInner());
